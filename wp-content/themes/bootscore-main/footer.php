42adaeadcc96a552e5101c11fe0092b81bb8f160
osdeibi <osdeibi@gmail.com>
2021-11-09 20:09:59 -0500

    Footer style for theme

--- wp-content/themes/bootscore-main/footer.php
+++ wp-content/themes/bootscore-main/footer.php
@@ -14,7 +14,7 @@
 
 <footer>
 
-  <div class="bootscore-footer bg-light pt-5 pb-3">
+  <div class="bootscore-footer bg-dark pt-5 pb-3">
     <div class="container">
 
       <!-- Top Footer Widget -->
@@ -27,16 +27,49 @@
       <div class="row">
 
         <!-- Footer 1 Widget -->
-        <div class="col-md-6 col-lg-3">
-          <?php if (is_active_sidebar('footer-1')) : ?>
+        <div class="col-md-3 col-lg-3">
             <div>
-              <?php dynamic_sidebar('footer-1'); ?>
+                <img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/img/logo/logo-white.png" width="150" alt="logo" class="">
             </div>
-          <?php endif; ?>
         </div>
 
+          <div class="col-md-3 mt-4 mt-md-1 mb-4 mb-md-1 text-center order-md-last col-lg-3">
+              <div class="icons-footer">
+
+                                <span class="fa-stack fa-white" style="vertical-align: top;">
+                                    <i class="far fa-circle fa-stack-2x"></i>
+                                    <i class="fab fa-instagram fa-stack-1x"></i>
+                                </span>
+
+                  <span class="fa-stack fa-white" style="vertical-align: top;">
+                                    <i class="far fa-circle fa-stack-2x"></i>
+                                    <i class="fab fa-facebook-f fa-stack-1x"></i>
+                                </span>
+                  <span class="fa-stack fa-white" style="vertical-align: top;">
+                                    <i class="far fa-circle fa-stack-2x"></i>
+                                    <i class="fab fa-youtube fa-stack-1x"></i>
+                                </span>
+
+                  <span class="fa-stack fa-white" style="vertical-align: top;">
+                                    <i class="far fa-circle fa-stack-2x"></i>
+                                    <i class="fab fa-linkedin-in fa-stack-1x"></i>
+                                </span>
+
+                  <span class="fa-stack fa-white" style="vertical-align: top;">
+                                    <i class="far fa-circle fa-stack-2x"></i>
+                                    <i class="fab fa-twitch fa-stack-1x"></i>
+                                </span>
+
+                  <span class="fa-stack fa-white" style="vertical-align: top;">
+                                    <i class="far fa-circle fa-stack-2x"></i>
+                                    <i class="fab fa-spotify fa-stack-1x"></i>
+                                </span>
+
+              </div>
+          </div>
+
         <!-- Footer 2 Widget -->
-        <div class="col-md-6 col-lg-3">
+        <div class="col-md-2 col-lg-2 col-6">
           <?php if (is_active_sidebar('footer-2')) : ?>
             <div>
               <?php dynamic_sidebar('footer-2'); ?>
@@ -45,7 +78,7 @@
         </div>
 
         <!-- Footer 3 Widget -->
-        <div class="col-md-6 col-lg-3">
+        <div class="col-md-2 col-lg-2 col-6">
           <?php if (is_active_sidebar('footer-3')) : ?>
             <div>
               <?php dynamic_sidebar('footer-3'); ?>
@@ -54,16 +87,27 @@
         </div>
 
         <!-- Footer 4 Widget -->
-        <div class="col-md-6 col-lg-3">
-          <?php if (is_active_sidebar('footer-4')) : ?>
-            <div>
-              <?php dynamic_sidebar('footer-4'); ?>
+        <div class="col-md-2 col-lg-2">
+            <div class="contact-footer text-white">
+    <p>Copec en Línea<br>
+    Teléfono: 600 200 02 02</p>
+
+                <p>Oficinas Generales Copec Chile:<br>
+                    Isidora Goyenechea 2915, Las Condes, Santiago - Chile</p>
+
+                <p>Términos y condiciones</p>
+
             </div>
-          <?php endif; ?>
         </div>
         <!-- Footer Widgets End -->
 
+
       </div>
+
+        <!-- Footer 4 Widget -->
+        <!-- Footer Widgets End -->
+
+    </div>
 
       <!-- Bootstrap 5 Nav Walker Footer Menu -->
       <?php
@@ -82,9 +126,9 @@
     </div>
   </div>
 
-  <div class="bootscore-info bg-light text-muted border-top py-2 text-center">
+  <div class="bootscore-info bg-dark text-white py-2 text-center">
     <div class="container">
-      <small>&copy;&nbsp;<?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></small>
+      <small>&copy;&nbsp;Copyright 2021 Compañía de Petróleos de Chile Copec S. A. Todos los derechos reservados</small>
     </div>
   </div>
 
